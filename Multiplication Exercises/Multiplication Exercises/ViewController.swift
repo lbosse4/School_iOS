ef1f4954c6bf69861af0a812f1627412059755b0
Lauren Bosse <Lauren@net19pc140.cse.psu.edu>
2015-08-31 21:01:58 -0400

    Start button pressed - before randomly generating numbers

--- Multiplication Exercises/Multiplication Exercises/ViewController.swift
+++ Multiplication Exercises/Multiplication Exercises/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let numbPossibleAnswers = 4
+    var numbQuestionsCorrect = 0
+    var numbQuestionsAsked = 0
+    var correctAnswerProgressMessage = ""
+    
     @IBOutlet weak var multiplicandLabel: UILabel!
     @IBOutlet weak var multiplierLabel: UILabel!
-    
+    @IBOutlet weak var horizontalLineLabel: UILabel!
+    @IBOutlet weak var multiplicationSymbolLabel: UILabel!
+    @IBOutlet weak var answerChoicesSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var correctAnswerProgressLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         multiplicandLabel.hidden = true
         multiplierLabel.hidden = true
-        
+        horizontalLineLabel.hidden = true
+        multiplicationSymbolLabel.hidden = true
+        answerChoicesSegmentedControl.hidden = true
+        correctAnswerProgressLabel.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +39,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startButtonPressed(sender: AnyObject) {
+        if startButton.currentTitle == "Start"{
+            startButton.hidden = true
+            startButton.setTitle("Next", forState: .Normal)
+            
+        }
+    }
+    
+    func generateMultiplicationArgs() {
+        
+    }
+    
+    /* Correct code for naming the progress label
+    correctAnswerProgressMessage = "\(numbQuestionsCorrect)/\(numbQuestionsAsked) Questions Correct"
+    correctAnswerProgressLabel.text = correctAnswerProgressMessage
+    correctAnswerProgressLabel.hidden = false
+    */
 
 }
 
