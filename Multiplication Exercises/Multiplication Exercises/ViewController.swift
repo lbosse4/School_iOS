17a6aeb5d2e01067475851a3764d6c68d40319ac
Lauren Bosse <Lauren@net19pc140.cse.psu.edu>
2015-08-31 22:05:13 -0400

    Answer array populated, not shuffled, screen not updated

--- Multiplication Exercises/Multiplication Exercises/ViewController.swift
+++ Multiplication Exercises/Multiplication Exercises/ViewController.swift
@@ -12,17 +12,26 @@ class ViewController: UIViewController {
 
     let numbPossibleAnswers = 4
     //This is exclusive
-    let multiplicationArgsUpperBound : UInt32 = 16
+    let multiplicationArgsUpperBound: UInt32 = 16
+    let numbRoundsInGame = 10
+    let allowedDistanceFromRightAnswer: UInt32 = 5
+    
+    var roundNumber = 0
     var numbQuestionsCorrect = 0
     var numbQuestionsAsked = 0
     var randomMultiplicandValue : UInt32 = 0
     var randomMultiplierValue :UInt32 = 0
+    var currentAnswer = 0
+    
+    var answerChoicesArray = [0,0,0,0]
+    
     var multiplicandValueToText = ""
     var multiplierValueToText = ""
     var correctAnswerProgressMessage = ""
     
     @IBOutlet weak var multiplicandLabel: UILabel!
     @IBOutlet weak var multiplierLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var horizontalLineLabel: UILabel!
     @IBOutlet weak var multiplicationSymbolLabel: UILabel!
     @IBOutlet weak var answerChoicesSegmentedControl: UISegmentedControl!
@@ -35,6 +44,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         multiplicandLabel.hidden = true
         multiplierLabel.hidden = true
+        answerLabel.hidden = true
         horizontalLineLabel.hidden = true
         multiplicationSymbolLabel.hidden = true
         answerChoicesSegmentedControl.hidden = true
@@ -49,9 +59,25 @@ class ViewController: UIViewController {
     @IBAction func startButtonPressed(sender: AnyObject) {
         if startButton.currentTitle == "Start"{
             startButton.hidden = true
-            directionLabel.text = "Pick correct answer."
             startButton.setTitle("Next", forState: .Normal)
+        }
+        
+        if startButton.currentTitle == "Next"{
+            directionLabel.text = "Pick correct answer."
             generateMultiplicationArgs()
+            currentAnswer = Int(randomMultiplicandValue * randomMultiplierValue)
+            populateAnswerChoicesSegmentedControl(currentAnswer)
+            answerChoicesSegmentedControl.hidden = false
+            
+            roundNumber++
+            
+            if roundNumber >= numbRoundsInGame {
+                startButton.setTitle("Restart", forState: .Normal)
+                roundNumber = 0
+            }
+        }
+        if startButton.currentTitle == "Restart"{
+            startButton.setTitle("Start", forState: .Normal)
         }
     }
     
@@ -70,6 +96,14 @@ class ViewController: UIViewController {
         multiplicationSymbolLabel.hidden = false
         horizontalLineLabel.hidden = false
     }
+    
+    func populateAnswerChoicesSegmentedControl(answer:Int) {
+        answerChoicesArray[0] = answer
+        answerChoicesArray[1] = answer + Int(arc4random_uniform(allowedDistanceFromRightAnswer)) + 1
+        answerChoicesArray[2] = answer + Int(arc4random_uniform(allowedDistanceFromRightAnswer)) + 1
+        answerChoicesArray[3] = answer + Int(arc4random_uniform(allowedDistanceFromRightAnswer)) + 1
+    }
+    
     
     /* Correct code for naming the progress label
     correctAnswerProgressMessage = "\(numbQuestionsCorrect)/\(numbQuestionsAsked) Questions Correct"
