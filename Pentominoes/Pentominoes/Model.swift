ff4e61a6f37aed5582ad908364d5d33906f11a39
Lauren Bosse <Lauren@Laurens-MacBook-Pro.local>
2015-09-15 18:29:42 -0400

    Before I mess everything up. Pentominoes class added. Images not initialized

--- Pentominoes/Pentominoes/Model.swift
+++ Pentominoes/Pentominoes/Model.swift
@@ -12,10 +12,44 @@ import UIKit
 
 class Model {
     
-    func help(sender: AnyObject) -> String {
+    let numBoards = 6
+    let numPentominoesPieces = 12
+    
+    let tileLettersArray = ["F", "I", "L", "N", "P", "T", "U", "V", "W", "X", "Y", "Z"]
+    
+    class pentominoesPiece {
+        var image : UIImage
+        var numFlips : Int
+        var numRotations : Int
+        var initialX : CGFloat
+        var initialY : CGFloat
+        
+        init() {
+            image = UIImage(named: "tileF.png")!
+            numFlips = 0
+            numRotations = 0
+            initialX = 0.0
+            initialY = 0.0
+        }
+    }
+    
+    //var pentominoesArray : pentominoesPiece
+    
+    init () {
+        //pentominoesArray
+    }
+    
+    func generateBoardImageName(sender: AnyObject) -> String {
         let currentBoardTagPressed = sender.tag
         let currentBoardImageName = "Board\(currentBoardTagPressed).png"
         return currentBoardImageName
+    }
+    
+    func generatePentominoesPiece(arrayIndex: Int) -> pentominoesPiece {
+        let tempPentominoesPiece = pentominoesPiece()
+        let tempPentominoesImage = UIImage(named: "tile\(tileLettersArray[arrayIndex]).png")
+        tempPentominoesPiece.image = tempPentominoesImage!
+        return tempPentominoesPiece
     }
     
 }
--- Pentominoes/Pentominoes/ViewController.swift
+++ Pentominoes/Pentominoes/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var boardImageView: UIImageView!
+    @IBOutlet weak var petominoesContainerView: UIView!
     
     let model = Model()
     
@@ -24,11 +25,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func boardButtonPressed(sender: AnyObject) {
-        let currentBoardImageName = model.help(sender)
-        boardImageView.image = UIImage(named: currentBoardImageName)
-        
+    override func viewDidLayoutSubviews() {
+        for i in 0...model.numPentominoesPieces - 1 {
+            model.generatePentominoesPiece(i)
+        }
     }
+    
+    @IBAction func boardButtonPressed(sender: AnyObject) {
+        let currentBoardImageName = model.generateBoardImageName(sender)
+        boardImageView.image = UIImage(named: currentBoardImageName)
+    }
+    
+    
+    /*
+    for i in 0..<lionImageCount {
+        let name = "Lion\(i).jpg"
+        let image = UIImage(named: name)
+        if let myimage = image {
+            let imageView = UIImageView(image: myimage)
+            imageView.frame = CGRectZero
+            imageView.contentMode = UIViewContentMode.ScaleAspectFit
+            imageView.autoresizingMask = UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleWidth
+    
+            imageViews.append(imageView)
+    
+        }
+    }
+    */
     
     @IBAction func resetButtonPressed(sender: AnyObject) {
     
