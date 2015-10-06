f88a96d592470c346e7b01cd95c2138b8dc40dbd
Lauren Bosse <Lauren@laurens-macbook-pro.local>
2015-10-06 15:01:20 -0400

    Thumbnails for collecion/table working. Sections are collapsable

--- Park Explorer/Park Explorer/ParkTableViewController.swift
+++ Park Explorer/Park Explorer/ParkTableViewController.swift
@@ -11,6 +11,20 @@ import UIKit
 class ParkTableViewController: UITableViewController {
     let model = Model.sharedInstance
     
+    var collapsedSections = [Bool]()
+    
+    override func viewDidLoad() {
+        for _ in 0..<model.numberOfParks() {
+            collapsedSections.append(false)
+        }
+    }
+    
+    func collapseSection(sender: UIButton){
+        collapsedSections[sender.tag] = !collapsedSections[sender.tag]
+        let indexSet = NSIndexSet(index: sender.tag)
+        tableView.reloadSections(indexSet, withRowAnimation: .Automatic)
+    }
+    
     // MARK: - Table view data source
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return model.numberOfParks()
@@ -19,7 +33,12 @@ class ParkTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return model.imageCountForPark(section)
+        if collapsedSections[section] {
+            return 0
+        } else {
+            return model.imageCountForPark(section)
+        }
+        
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -30,7 +49,10 @@ class ParkTableViewController: UITableViewController {
         
         let image = UIImage(named: "\(model.imageNameAtIndexPath(indexPath)).jpg")
         
+        
+        
         cell.parkImageView.image = image
+        //cell.parkImageView.contentMode = UIViewContentMode.ScaleAspectFill
         
         /*
         if stateModel.isCheckedStateAtIndex(indexPath.row) {
@@ -43,11 +65,21 @@ class ParkTableViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return model.parkNameForSection(section)
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let button = UIButton(frame: CGRect(x: 0.0, y: 0.0, width: tableView.frame.width, height: 30.0))
+        button.setTitle(model.parkNameForSection(section), forState: .Normal)
+        button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        button.addTarget(self, action: "collapseSection:", forControlEvents: .TouchUpInside)
+        button.tag = section
+        return button
     }
     
+//    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return model.parkNameForSection(section)
+//    }
+    
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        collapsedSections[indexPath.section] = true
         //stateModel.toggleIsCheckedStateAtIndex(indexPath.row)
         //tableView.reloadData()
     }
