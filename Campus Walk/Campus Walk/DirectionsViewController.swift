8fcf7618e0924f8dc181a5958a64a3c73d156110
Lauren Bosse <Lauren@net19pc187.cse.psu.edu>
2015-10-26 20:09:57 -0400

    Directions UI worked out

--- Campus Walk/Campus Walk/DirectionsViewController.swift
+++ Campus Walk/Campus Walk/DirectionsViewController.swift
@@ -23,8 +23,10 @@ class DirectionsViewController : UIViewController, FindBuildingsProtocol {
     @IBOutlet weak var getDirectionsView: UIView!
     @IBOutlet weak var chooseABuildingButton: UIButton!
     @IBOutlet weak var chooseABuildingView: UIView!
+    @IBOutlet weak var directionsConfirmationLabel: UILabel!
     
     let model = Model.sharedInstance
+    let deselectIndex : Int = -1
     var delegate : GetDirectionsProtocol?
     var chosenBuildingFromTableView : Building?
     var source : Building?
@@ -43,34 +45,44 @@ class DirectionsViewController : UIViewController, FindBuildingsProtocol {
         self.delegate?.cancelChildViewController()
     }
     
+    @IBAction func getDirectionsButtonPressed(sender: UIButton) {
+        //delegate?.buildingSourceAndDestinationSelected(source, destination: destination)
+    }
+    
     @IBAction func toFromSegmentedControlTriggered(sender: UISegmentedControl) {
         locationOrBuildingSegmentedControl.hidden = false
         responseLabel.hidden = false
         switch sender.selectedSegmentIndex {
         //to
         case 0:
-            responseLabel.text = "Should the directions start from your \n current location or another building on campus?"
+            responseLabel.text = "Should the directions start from your current location or another building on campus?"
             destination = building
             //isEndingAtBuilding = true
             isStartingFromBuilding = false
         //from
         case 1:
-            responseLabel.text = "Should the directions end at your \n current location or another building on campus?"
+            responseLabel.text = "Should the directions end at your current location or another building on campus?"
             source = building
             isStartingFromBuilding = true
         default:
             break
         }
+        updateDirectionsConfirmationLabel()
+        directionsConfirmationLabel.hidden = true
+        locationOrBuildingSegmentedControl.selectedSegmentIndex = deselectIndex
+        getDirectionsButton.hidden = true
+        getDirectionsView.hidden = true
     }
     
     @IBAction func locationOrBuildingSegmentedControlTriggered(sender: UISegmentedControl) {
+        directionsConfirmationLabel.hidden = true
         switch sender.selectedSegmentIndex {
         //location
         case 0:
             chooseABuildingButton.hidden = true
             chooseABuildingView.hidden = true
             //sending back as a building object for simplicity reasons. Only care about the title
-            let userLocationBuildingObject = Building(title: "sender", coordinate: CLLocationCoordinate2D(), subtitle: "")
+            let userLocationBuildingObject = Building(title: "your current location", coordinate: CLLocationCoordinate2D(), subtitle: "")
             if isStartingFromBuilding {
                 destination = userLocationBuildingObject
             } else {
@@ -78,6 +90,7 @@ class DirectionsViewController : UIViewController, FindBuildingsProtocol {
             }
             getDirectionsView.hidden = false
             getDirectionsButton.hidden = false
+            updateDirectionsConfirmationLabel()
         //building
         case 1:
             chooseABuildingView.hidden = false
@@ -91,13 +104,10 @@ class DirectionsViewController : UIViewController, FindBuildingsProtocol {
         
     }
     @IBAction func chooseABuildingButtonPressed(sender: UIButton) {
-        let bldgSearchViewController = self.storyboard?.instantiateViewControllerWithIdentifier("FindBuildingTableViewController") as! FindBuildingTableViewController
+        let findBuildingViewController = self.storyboard?.instantiateViewControllerWithIdentifier("FindBuildingTableViewController") as! FindBuildingTableViewController
         
-        bldgSearchViewController.delegate = self
-        
-        // present the view controller
-        self.presentViewController(bldgSearchViewController, animated: true, completion: nil)
-        
+        findBuildingViewController.delegate = self
+        self.presentViewController(findBuildingViewController, animated: true, completion: nil)
     }
 
     func findBuildingViewControllerDismissed(chosenBuilding : Building){
@@ -110,7 +120,16 @@ class DirectionsViewController : UIViewController, FindBuildingsProtocol {
         } else {
             source = chosenBuildingFromTableView
         }
-        
+        updateDirectionsConfirmationLabel()
+    }
+    
+    func updateDirectionsConfirmationLabel() {
+        if let sourceTitle = source?.title {
+            if let destinationTitle = destination?.title {
+                directionsConfirmationLabel.text = "Click below for directions to \(destinationTitle) from \(sourceTitle)."
+                directionsConfirmationLabel.hidden = false
+            }
+        }
     }
     
 }
