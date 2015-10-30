8f2b7a2df8242fca49c7d2e080e93b227a9175c1
Lauren Bosse <Lauren@net19pc163.cse.psu.edu>
2015-10-30 01:42:50 -0400

    Lots of UI

--- GameTime Stats/GameTime Stats/ViewController.swift
+++ GameTime Stats/GameTime Stats/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //square.layer.cornerRadius = 1/2 of length
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
