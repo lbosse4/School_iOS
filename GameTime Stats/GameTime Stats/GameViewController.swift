f37f0e32ff2e2120bd96365b6ea063be3e4b00c0
Lauren Bosse <Lauren@laurens-macbook-pro.local>
2015-11-15 13:00:55 -0500

    Back to where I started!!!!!

--- GameTime Stats/GameTime Stats/GameViewController.swift
+++ GameTime Stats/GameTime Stats/GameViewController.swift
@@ -8,23 +8,24 @@
 
 import UIKit
 
-class GameViewController : UIViewController {
+class GameViewController : UIViewController, UIGestureRecognizerDelegate {
     let model = Model.sharedInstance
     let darkBlueColor = UIColor(red: 0.01, green: 0.02, blue: 0.78, alpha: 1.0)
     let playerViewSize : CGFloat = 55.0
     let playerViewPaddingWidth : CGFloat = 60.0
     let playerViewMargin : CGFloat = 10.0
     let playerNumberFont = "collegiateHeavyOutline"
+    let animationDuration : NSTimeInterval = 0.55
     
     var playerViews = [UIView]()
-    var testPlayers = [Player]()
+    var currentPlayers = [Player]()
     
     @IBOutlet weak var benchContainerView: UIView!
     @IBOutlet weak var fieldImageView: UIImageView!
     
     override func viewDidLoad() {
-        testPlayers = model.tstPlayers()
-        testPlayers.sortInPlace{(Int($0.jerseyNumber!) < Int($1.jerseyNumber!))}
+        currentPlayers = model.tstPlayers()
+        currentPlayers.sortInPlace{(Int($0.jerseyNumber!) < Int($1.jerseyNumber!))}
         fieldImageView.userInteractionEnabled = true
         benchContainerView.userInteractionEnabled = true
         //square.layer.cornerRadius = 1/2 of length
@@ -34,11 +35,14 @@ class GameViewController : UIViewController {
     
     //MARK: Helper Functions
     func generateViews() {
-        for player in testPlayers {
+        var loopCounter = 0
+        
+        for player in currentPlayers {
             let playerFrame = CGRect(x: playerViewMargin + (playerViewPaddingWidth * CGFloat(player.jerseyNumber!)), y: playerViewMargin, width: playerViewSize, height: playerViewSize)
             let playerView = UIView(frame: playerFrame)
             playerView.backgroundColor = darkBlueColor
             playerView.layer.cornerRadius = playerViewSize/2
+            playerView.tag = loopCounter
             benchContainerView.addSubview(playerView)
             
             let playerNumber = player.jerseyNumber!
@@ -49,6 +53,63 @@ class GameViewController : UIViewController {
             playerNumberLabel.textColor = UIColor.whiteColor()
             playerNumberLabel.textAlignment = .Center
             playerView.addSubview(playerNumberLabel)
+            
+            let panRecognizer = UIPanGestureRecognizer(target: self, action: "panningPlayer:")
+            playerView.addGestureRecognizer(panRecognizer)
+            
+            loopCounter += 1
+        }
+    }
+    
+    func checkFieldViewBounds(currentLocation: CGPoint) -> Bool {
+        return (currentLocation.x > fieldImageView.bounds.width) || (currentLocation.x < 0) || (currentLocation.y > fieldImageView.bounds.height) || (currentLocation.y < 0)
+    }
+    
+    func resetPlayerView(playerView: UIView) {
+        moveView(playerView, toSuperview: benchContainerView)
+        
+        UIView.animateWithDuration(animationDuration, animations: { () -> Void in
+            let origin = CGPoint(x: self.playerViewMargin + (self.playerViewPaddingWidth * CGFloat(playerView.tag)), y: self.playerViewMargin)
+            playerView.frame.origin = origin
+        })
+        benchContainerView.addSubview(playerView)
+    }
+    
+    func moveView(view:UIView, toSuperview superView: UIView) {
+        let newCenter = superView.convertPoint(view.center, fromView: view.superview)
+        view.center = newCenter
+        superView.addSubview(view)
+    }
+    
+    //MARK: Gesture Recognizers
+    func panningPlayer(recognizer: UIGestureRecognizer) {
+        if let panningView = recognizer.view {
+            fieldImageView.addSubview(panningView)
+            let origin = benchContainerView.convertPoint(recognizer.locationInView(fieldImageView), fromView: benchContainerView)
+            panningView.center = origin
+            
+            switch recognizer.state {
+            case .Ended:
+                let currentLocation = panningView.frame.origin
+                if checkFieldViewBounds(currentLocation) {
+                    resetPlayerView(panningView)
+                } else {
+                    
+                    
+                }
+//                if checkBoardViewBounds(currentLocation) {
+//                    resetPentominoView(panningImageView, index: index)
+//                } else {
+//                    let snapOrigin : CGPoint = findSnapCoordinates(currentLocation);
+//                    boardImageView.addSubview(panningImageView)
+//                    //UIView.animateWithDuration(animationDuration, animations: { () -> Void in
+//                    panningImageView.frame.origin = CGPoint(x: snapOrigin.x, y: snapOrigin.y)
+//                    //})
+//                }
+
+            default:
+                break
+            }
         }
     }
     
