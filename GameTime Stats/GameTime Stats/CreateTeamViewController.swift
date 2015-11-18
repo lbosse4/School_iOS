bc99954f82cc9bb450bd7d4c05bd38611e5746a8
Lauren Bosse <Lauren@net19pc154.cse.psu.edu>
2015-11-18 00:31:40 -0500

    More changes

--- GameTime Stats/GameTime Stats/CreateTeamViewController.swift
+++ GameTime Stats/GameTime Stats/CreateTeamViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class CreateTeamViewController : UIViewController {
+class CreateTeamViewController : UIViewController, UITextFieldDelegate {
     let model = Model.sharedInstance
+    @IBOutlet weak var teamNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        teamNameTextField.delegate = self
+    }
     
     @IBAction func cancelButtonPressed(sender: UIButton) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    //MARK: TextField Delegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
